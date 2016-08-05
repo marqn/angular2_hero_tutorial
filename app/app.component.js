4fdf866e761b43746ffa7003b6c81a7e800e611b
marcin.matula <marqnpl@gmail.com>
2016-08-05 15:51:56 +0200

    a simple hero editor

--- app/app.component.js
+++ app/app.component.js
@@ -9,14 +9,29 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
-        this.test = 'hello world';
+        this.title = 'Tour of Heroes';
+        this.hero = {
+            id: 1,
+            name: 'marqn'
+        };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App {{test}}</h1>'
+            template: '<h1>{{title}}</h1>' +
+                '<h2>{{hero.name}} details:</h2>' +
+                '<div><label>id: {{hero.id}}</label></div>' +
+                '<div><label>name: {{hero.name}}</label>' +
+                '<input [(ngModel)]="hero.name" placeholder="name">' +
+                '</div>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export class Hero {
+    id:number;
+    name:string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App {{test}}</h1>'
+    template: '<h1>{{title}}</h1>' +
+    '<h2>{{hero.name}} details:</h2>' +
+    '<div><label>id: {{hero.id}}</label></div>' +
+    '<div><label>name: {{hero.name}}</label>' +
+    '<input [(ngModel)]="hero.name" placeholder="name">' +
+    '</div>'
 })
 export class AppComponent {
-    test = 'hello world';
+    title = 'Tour of Heroes';
+    hero:Hero = {
+        id: 1,
+        name: 'marqn'
+    };
 }
+
